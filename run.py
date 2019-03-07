70d14549a89749f101edd3efef7646d7414d326e
adapap <adapap55555@gmail.com>
2019-03-07 00:52:15 -0500

    Heroku setup

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import create_app
-from flask_frozen import Freezer
 
 app = create_app()
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
+"""WSGI callable application."""
+application = app
